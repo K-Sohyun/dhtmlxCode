4ce47ec31cdc2a72e8445acb6abc5ccf48fed193
DESKTOP-BSVGB20\sohyun <ksoh912@daonlink.com>
2025-02-07 12:51:34 +0900

    실습 코드 Commit

---
+++ js/common.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const urlParams = new URL(location.href).searchParams;
+    const pageName = urlParams.get('page');
+    console.log(pageName);
+
+    const menuEls = document.querySelectorAll('#sidebarArea button');
+    console.log(menuEls);
+
+    // menuEls.forEach(function(menuEl) {
+    //     menuEl.classList.add('on');
+    // });
+});
