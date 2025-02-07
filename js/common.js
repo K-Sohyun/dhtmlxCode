913623a44d0d05c81ec62d42d79e2a262598cd76
DESKTOP-BSVGB20\sohyun <ksoh912@daonlink.com>
2025-02-07 14:00:32 +0900

    콘솔로그 주석

--- js/common.js
+++ js/common.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     const urlParams = new URL(location.href).searchParams;
     const pageName = urlParams.get('page');
-    console.log(pageName);
+    // console.log(pageName);
 
     const menuEls = document.querySelectorAll('#sidebarArea button');
-    console.log(menuEls);
+    // console.log(menuEls);
 
     // menuEls.forEach(function(menuEl) {
     //     menuEl.classList.add('on');
